4aa06f1cb6d7cfcae77ab9b0480f393807b18b1e
Ngo Thanh Tung <tungnt@softech.vn>
2017-11-15 18:15:53 +0700

    Update Validator

--- routes/api.js
+++ routes/api.js
@@ -3,12 +3,21 @@ var router = express.Router();
 var db = require('../helpers/MongoDbHelper');
 var authenticate = require('../helpers/AuthenticateHelper');
 
+var validator = require('validator');
+
 // ---------------------------------------------------------
 // authentication (no middleware necessary since this isnt authenticated)
 // ---------------------------------------------------------
 router.post('/authenticate', function (req, res) {
     // check login here   
     authenticate.login(req, res);
+});
+
+router.post('/product', function (req, res) {
+    var data = req.body;
+    var check = validator.isEmail('tungnt$softech.vn');
+    console.log(check);
+    res.send("OK");
 });
 
 // ---------------------------------------------------------
@@ -22,31 +31,35 @@ router.use(function (req, res, next) {
 // authenticated routes
 // ---------------------------------------------------------
 router.get('/', function (req, res) {
-    res.json({ message: 'Welcome to the coolest API on earth!' });
+    res.json({
+        message: 'Welcome to the coolest API on earth!'
+    });
 });
 
 router.get('/users', function (req, res) {
-    db.findDocuments({}, "users", function(result) {
+    db.findDocuments({}, "users", function (result) {
         res.json({
             success: true,
             message: "OK",
             token: "",
             data: result
         });
-    })  
+    })
 });
 
 
 router.get('/products', function (req, res) {
-    db.findDocuments({}, "products", function(result) {
+    db.findDocuments({}, "products", function (result) {
         res.json({
             success: true,
             message: "OK",
             token: "",
             data: result
         });
-    })  
+    })
 });
+
+
 
 router.get('/check', function (req, res) {
     res.json(req.decoded);
